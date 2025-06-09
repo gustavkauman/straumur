0dc0f7e4049d992811c04b4cc9a3abd0e7f20b85
Gustav Utke Kauman <gustav@kauman.dk>
2025-06-09 17:26:09 +0200

    re-enable sentry

--- apps/frontend/functions/_middleware.js
+++ apps/frontend/functions/_middleware.js
@@ -2,13 +2,17 @@ import * as Sentry from "@sentry/cloudflare";
 
 export const onRequest = [
     // Sentry MUST be the first middleware
-    /*
-    Sentry.sentryPagesPlugin((context) => ({
-        dsn: context.cloudflare.env.SENTRY_DSN,
-        enabled: context.cloudflare.env.SENTRY_ENABLED,
-        tracePropagationTargets: ["localhost"],
-        tracesSampleRate: 1.0,
-    })),
-    */
+    Sentry.sentryPagesPlugin((context) => {
+        const env = context.env;
+        const { id: versionId } = env.CF_VERSION_METADATA;
+        
+        return {
+            dsn: env.SENTRY_DSN,
+            release: versionId,
+            enabled: env.SENTRY_ENABLED,
+            tracePropagationTargets: ["localhost", "straumur.app"],
+            tracesSampleRate: 1.0,
+        };
+    })
 ];
 
